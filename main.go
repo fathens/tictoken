be12f25175809fd682b17f302ed7640884080077
沢谷 邦夫 <fsawa@fathens.org>
2021-10-14 20:56:14 +0900

    コマンドライン引数で挙動を分けるようにした

--- main.go
+++ main.go
@@ -20,11 +20,13 @@ func main() {
 	hdpath := flag.String("hdpath", wallet.DefaultPath, "HDPath")
 	solc := flag.String("solc", "solc", "solc command")
 	flag.Parse()
-	args := flag.Args()
-	if len(args) < 1 {
-		panic("No filename supplied.")
+	fullArgs := flag.Args()
+	if len(fullArgs) < 1 {
+		panic("command name (deploy or invoke) must be supplied.")
 	}
-	fileName := args[0]
+	cmd := fullArgs[0]
+	args := fullArgs[1:]
+
 	cfg := readConfig(*configFile)
 	fmt.Println("config =", cfg)
 
@@ -32,11 +34,11 @@ func main() {
 	account := setupAccount(mnemonic, *hdpath)
 	fmt.Println(account.Address())
 
-	contractAddr, err := dapp.DeployFromSrc(account, *solc, fileName, "", "")
-	if err != nil {
-		panic(err)
+	switch cmd {
+	case "deploy": deploy(cfg, account, *solc, args)
+	case "invoke": invoke(cfg, account, args)
+	default: panic(fmt.Sprintf("Unsupported command: %v", cmd))
 	}
-	fmt.Println(contractAddr)
 }
 
 func readConfig(path string) Config {
@@ -63,4 +65,24 @@ func setupAccount(mnemonic, hdpath string) wallet.Account {
 		panic(err)
 	}
 	return account
+}
+
+func deploy(config Config, account wallet.Account, solc string, args []string) {
+	fmt.Println("Exec deploy command: ", args)
+	if len(args) < 3 {
+		panic("filename, tokenName and tokenSymbol must be supplied.")
+	}
+	fileName := args[0]
+	tokenName := args[1]
+	tokenSymbol := args[2]
+
+	contractAddr, err := dapp.DeployFromSrc(account, solc, fileName, tokenName, tokenSymbol)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(contractAddr)
+}
+
+func invoke(config Config, account wallet.Account, args []string) {
+	fmt.Println("Exec invoke command: ", args)
 }
