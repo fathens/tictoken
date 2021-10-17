4223b4ab041273b5a3b0cf7e677038b9d021e3aa
沢谷 邦夫 <fsawa@fathens.org>
2021-10-17 16:00:43 +0900

    `invokeContract` の実装

--- dapp/token.go
+++ dapp/token.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
@@ -96,4 +97,66 @@ func deployContract(
 	fmt.Printf("Tx %v, Contract %v\n", tx, bound)
 
 	return &contractAddr, nil
+}
+
+func Invoke(
+	rpcserver string,
+	account wallet.Account,
+	contractAddress common.Address,
+	method string,
+	abiJsonFile string,
+	args []string,
+) (*[]interface{}, error) {
+	reader, err := os.Open(abiJsonFile)
+	if err != nil {
+		return nil, err
+	}
+	abiInfo, err := abi.JSON(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	m := abiInfo.Methods[method]
+
+	params := make([]interface{}, len(args))
+	for i, s := range args {
+		switch m.Inputs[i].Type.T {
+		case abi.AddressTy:
+			params[i] = common.HexToAddress(s)
+		case abi.StringTy:
+			params[i] = s
+		default:
+			params[i] = s
+		}
+	}
+
+	return invokeContract(rpcserver, account.Address(), contractAddress, method, abiInfo, params...)
+}
+
+func invokeContract(
+	rpcserver string,
+	account, address common.Address,
+	method string,
+	abiInfo abi.ABI,
+	params ...interface{},
+) (*[]interface{}, error) {
+	client, err := ethclient.Dial(rpcserver)
+	if err != nil {
+		return nil, err
+	}
+
+	opts := bind.CallOpts{
+		From:    account,
+		Context: context.Background(),
+	}
+	fmt.Println("Client:", client, opts, params)
+
+	c := bind.NewBoundContract(address, abiInfo, client, client, client)
+
+	results := make([]interface{}, 0)
+	err = c.Call(&opts, &results, method, params...)
+	if err != nil {
+		return nil, err
+	}
+	return &results, nil
 }
